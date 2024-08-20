83e330f29689c0d298716d5873cbef32010581b2
AlphaMoonbaseBerlin <kontakt@alphamoonbase.de>
2024-08-20 21:06:13 +0200

    New API and functionality for dictParser

--- modules/suspects/project/dictParser/extDictParser.py
+++ modules/suspects/project/dictParser/extDictParser.py
@@ -2,7 +2,7 @@
 Name : extDictParser
 Author : Wieland@AMB-ZEPH15
 Saveorigin : Project.toe
-Saveversion : 2022.32660
+Saveversion : 2022.35320
 Info Header End'''
 
 
@@ -19,6 +19,10 @@ class extDictParser:
 	def __init__(self, ownerComp):
 		# The component to which this extension is attached
 		self.ownerComp = ownerComp
+
+		# Backwards Compatible!
+		self.AddDict = self.AddItem
+		self.AddDicts = self.AddItems
 		self.Refresh()
 
 	@property
@@ -27,7 +31,7 @@ class extDictParser:
 	
 	@property
 	def dataTable(self) -> tableDAT:
-		return self.ownerComp.op("data") 
+		return self.ownerComp.op("repoMaker").Repo 
 	
 	def Refresh(self):
 		self.getDefintion.cache_clear()
@@ -49,36 +53,87 @@ class extDictParser:
 			self.dataTable.appendRow([
 				definitionItem.name for definitionItem in definitionList
 			])
-		return definitionList
+		return { item.name : item for item in definitionList }
 	
-	def AddDict(self, data:dict, unique = True):
-		dataset = [ item.parse( data ) for item in self.getDefintion() ]
+	def AddItem(self, data:dict, unique = True):
+		"""
+			Passes a dict to the parser-functions and saves it to the table.
+			When unique is enabled it will overwrite existing entries with the same
+			primary key.
+		"""
+		dataset = [ item.parse( data ) for item in self.getDefintion().values() ]
 		if not dataset: raise Exception("No Definition defined!")
 		if unique and self.dataTable.row( dataset[0] ):
 			self.dataTable.replaceRow( dataset[0], dataset )
 		else:
 			self.dataTable.appendRow( dataset )
 
-	def AddDicts(self, data:List[dict], unique = True):
+	def UpdateItem(self, itemId:Union[str, int], dataset:dict):
+		"""
+			Replaces the data in the table at the given ID with the dataset given.
+		"""
+		self.dataTable.replaceRow( itemId, dataset )
+
+	def AddItems(self, data:List[dict], unique = True):
+		"""
+			Ads a list of dicts.
+		"""
 		for item in data:
-			self.AddDict( item, unique=unique )
+			self.Additem( item, unique=unique )
 
 
 	def GetRow(self, rowIndex:entryIndex):
+		"""Deprecated, used GetItem instead"""
 		return {
-			item.name : item.unparse( str( self.outputTable[ rowIndex, item.name] ) ) for item in self.getDefintion() 
+			item.name : item.unparse( str( self.outputTable[ rowIndex, item.name] ) ) for item in self.getDefintion().values() 
 		}
 	
-	def GetValue(self, rowIndex:entryIndex, itemIndex:entryIndex) -> Dict[str, any]:
-		"""Return a dictionary of the parsed row!"""
-		targetCell:Cell = self.outputTable[ rowIndex, itemIndex ]
-		if not targetCell: return None
-		entryDefinition:EntryDefinition = self.getDefintion[targetCell.col]
-		return entryDefinition.unparse( targetCell.val )
+	def GetItem(self, itemId:str, rows = "*") -> dict:
+		"""
+			Get a single items as a dict.
+		"""
+		itemDefinition = self.getDefintion()
+		fetchNames = tdu.match(
+			rows, list(itemDefinition.keys())
+		)
+		result = {
+			item.name : item.unparse( 
+				str( self.outputTable[ itemId, item.name] ) 
+			) for item in itemDefinition.values() if item.name in fetchNames
+		}
+		result["_tableIndex"] = self.outputTable[ itemId, 0].row
+		return result
 	
-	def SearchRows(self, searchValue:str, searchCol:entryIndex) -> List[entryIndex]:
-		"""This loads the complete row, which is def not ideal, but usable. Use Index-Tables for quicker search?"""
-		foundCells = [result for result in tdu.match(searchValue, self.outputTable.col(searchCol)[1:] )]
-		return [
-			self.outputTable[ cell.row, 0].val for cell in foundCells
-		]
+	def DeleteItem(self, itemId:str):
+		"""
+			Delete an item based on the ID. Returns True or False depending of success.
+		"""
+		try:
+			self.dataTable.deleteRow( str(itemId) )
+			return True
+		except tdError:
+			return False
+
+	def SearchItems(self, key = lambda value: True, rows = "*") -> List[Dict]:
+		"""
+			Search Items in the table based on the key-function returning true or false.
+			Use rows arguments to filter returned data.
+		"""
+		returnData = []
+		for idCell in self.dataTable.col(0)[1:]:
+			item = self.GetItem( idCell.val, rows = rows )
+			if key(item): returnData.append( item )
+		return returnData
+
+	def SortTable(self, key = lambda row: row[0], reverse = False) -> bool:
+		"""
+			Sorts the table based on the key-function. This works on the table
+			holding the data and does not change the data.
+		"""
+		sortedData = sorted(
+    		[[cell.val for cell in row] for row in self.dataTable.rows()[1:]], 
+			key = key,
+			reverse=reverse
+		)
+		self.dataTable.clear(keepFirstRow = True)
+		self.dataTable.appendRows( sortedData )
